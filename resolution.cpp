201a1b41441738d1f6f3595812fea1f400ef26c2
nymanelin <elin.nyman@aalto.fi>
2018-06-14 10:03:29 +0200

    Fixed merge conflict

--- resolution.cpp
+++ resolution.cpp
@@ -43,16 +43,6 @@ int main(int argc, char **pargv){
     }
   }
 
-  for (uint i = 0; i < N_VN+1; ++i) {
-    high_v[i] =  new TGraphErrors();
-    for (uint c = 0; c < NC; ++c) {
-      TH1D *temp_hist[5];
-      for (int ilbl = 0; ilbl < 3; ilbl++) {
-        temp_hist[ilbl] = (TH1D*)highf->Get(Form("Table %u/%s", ilbl+1, Form(pglabel[ilbl], c+1)));
-      }  // y1 : v2, y2 : v2{4}, y3 : v3;
-    }
-  }
-
   pff->Close();
   delete pff;
 
@@ -123,35 +113,23 @@ int main(int argc, char **pargv){
       if(cent < CentBins[cid+1])
         break;
 
-    //Q-vectors -----------------------------------
+    //Q-vectors Only for 2nd order-----------------------------------
     double trueevp = pdf->GetParameter(6);
 
-    //containers of Pure flow tracks and jet tracks. Do we use jet tracks?
+    //containers of Pure flow tracks and jet tracks
     std::vector <double> trackphi[D_COUNT];
-    std::vector <double> tracketa[D_COUNT];
-    std::vector <double> jettrackphi[D_COUNT];
-    std::vector <double> jettracketa[D_COUNT];
-    TComplex Qsd[D_COUNT];
+    TComplex Qsd[D_COUNT]; // QVector per each detector 2nd order only
+    // Calculating QVector per each detector
     for(uint s = 0; s < D_COUNT; ++s){
       uint ntracks = (uint)pgr_nch[s]->Eval(cent) *0.9;
 
       TComplex Qa2 = TComplex(0,0);
       for(uint i = 0; i < ntracks; ++i){
         double tphi = pdf->GetRandom();
-        //				phi = TMath::Floor(8.0*(phi+pi)/(2.0*pi))*(2.0*pi)/8-pi;
         double phi = CheckDetectorPhi(tphi);
-        //        double phi = tphi;
         trackphi[s].push_back(phi);
-        tracketa[s].push_back(prng->Uniform(-0.8, 0.8));
         Qa2 += TComplex(TMath::Cos(2.0*phi),TMath::Sin(2.0*phi));
       }
-      
-      //      double thphi = pdf_high->GetRandom();
-      //      double hphi = CheckDetectorPhi(thphi);
-      //      trackphi[s].push_back(hphi);
-      //			Qa2 += TComplex(TMath::Cos(2.0*hphi),TMath::Sin(2.0*hphi));
-
-      //		Qa2 /= (double) (ntracks+1);   // for highpt
       Qa2 /= (double) (ntracks);
       Qsd[s] = Qa2/TComplex::Abs(Qa2);
     }
@@ -159,32 +137,15 @@ int main(int argc, char **pargv){
     //Calculate Event plane using Q Vectors
 
     for (uint s = 0; s < R_COUNT; ++s) {
-      double recoevp = TMath::ATan2(Qsd[s].Im(), Qsd[s].Re())/2; // What is recoevp
+      double recoevp = TMath::ATan2(Qsd[s].Im(), Qsd[s].Re())/2; 
       double evpdiff = trueevp - recoevp;
 
-      evph[s][cid]->Fill(recoevp);
-      evpdifference[s][cid]->Fill(evpdiff);
+      evph[s][cid]->Fill(recoevp); // reconsructed EP
+      evpdifference[s][cid]->Fill(evpdiff); // for resolution
     }
 
-    //    evpcorrvsdet2d[cid]->Fill(TMath::ATan2(Qsd[0].Im(), Qsd[0].Re()), TMath::ATan2(Qsd[1].Im(), Qsd[1].Re()));
-
-
-    //Calculate the resolution components
+    //Calculate the resolution components with different methods.
     TComplex ab, ac, bc;
-    /*ab = Qsd[D_V0A]*TComplex::Conjugate(Qsd[D_TPC]);
-      ac = Qsd[D_V0A]*TComplex::Conjugate(Qsd[D_V0C]);
-      bc = Qsd[D_TPC]*TComplex::Conjugate(Qsd[D_V0C]);
-      pah[R_V0A][cid]->Fill(ab.Re());
-      pbh[R_V0A][cid]->Fill(ac.Re());
-      pch[R_V0A][cid]->Fill(bc.Re());
-
-      ab = Qsd[D_V0C]*TComplex::Conjugate(Qsd[D_V0A]);
-      ac = Qsd[D_V0C]*TComplex::Conjugate(Qsd[D_TPC]);
-      bc = Qsd[D_V0A]*TComplex::Conjugate(Qsd[D_TPC]);
-      pah[R_V0C][cid]->Fill(ab.Re());
-      pbh[R_V0C][cid]->Fill(ac.Re());
-      pch[R_V0C][cid]->Fill(bc.Re());*/
-    // What does this part do?
     ab = Qsd[D_V0A]*TComplex::Conjugate(Qsd[D_TPC_ETAA]);
     ac = Qsd[D_V0A]*TComplex::Conjugate(Qsd[D_TPC_ETAC]);
     bc = Qsd[D_TPC_ETAA]*TComplex::Conjugate(Qsd[D_TPC_ETAC]);
@@ -194,7 +155,6 @@ int main(int argc, char **pargv){
 
     ab = Qsd[D_V0C]*TComplex::Conjugate(Qsd[D_TPC_ETAA]);
     ac = Qsd[D_V0C]*TComplex::Conjugate(Qsd[D_TPC_ETAC]);
-    //bc = Qsd[D_TPC_ETAA]*TComplex::Conjugate(Qsd[D_TPC]);
     pah[R_V0C][cid]->Fill(ab.Re());
     pbh[R_V0C][cid]->Fill(ac.Re());
     pch[R_V0C][cid]->Fill(bc.Re());
@@ -208,41 +168,27 @@ int main(int argc, char **pargv){
 
   }
 
+  // cleaning up the variables
   for(uint i = 0; i < D_COUNT; ++i)
     delete pgr_nch[i];
   for(uint i = 0; i < N_VN; ++i) {
     delete pgr_v[i];
-    delete high_v[i];
   }
 
   delete prng;
   delete pdf;
-  delete pdf_high;
 
   TFile *pfo = new TFile(argc > 3?pargv[3]:"results.root","recreate");
   pfo->cd();
 
-  // What does this part do?
   for(uint i = 0; i < R_COUNT; ++i){
     for(uint j = 0; j < NC; ++j){
-      //double a[2] = {pah[i]->GetMean(),pah[i]->GetMeanError()};
-      //double b[2] = {pbh[i]->GetMean(),pah[i]->GetMeanError()};
-      //double c[2] = {pch[i]->GetMean(),pah[i]->GetMeanError()};
-      //
-      //double R = TMath::Sqrt(a[0]*b[0]/c[0]);
-      //double e = TMath::Sqrt(b[0]*a[1]*a[1]/(a[0]*c[0])
-      //	+a[0]*b[1]*b[1]/(b[0]*c[0])+a[0]*b[0]*c[1]/(c[0]*c[0]*c[0]));
-      //printf("R2(%u) = %lf pm %lf\n",i,R,e);
-
       pah[i][j]->Write(Form("h_%s_a%02u",presn[i],j));
       pbh[i][j]->Write(Form("h_%s_b%02u",presn[i],j));
       pch[i][j]->Write(Form("h_%s_c%02u",presn[i],j));
 
       evph[i][j]->Write(Form("h_evp_%s_%02u", presn[i], j));
       evpdifference[i][j]->Write(Form("h_evpdiff_%s_%02u", presn[i], j));
-
-
-
 
       delete pah[i][j];
       delete pbh[i][j];
